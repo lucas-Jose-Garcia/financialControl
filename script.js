b62b0004040c11c59c65cd5968b61678fd015c58
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2022-09-02 20:31:26 -0300

    Imcorporando os Valores do array na tela

---
+++ script.js
@@ -0,0 +1,30 @@
+const transactionUl = document.querySelector('#transactions')
+
+
+const dummyTransaction = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20},
+    { id: 2, name: 'Salário', amount: 300},
+    { id: 3, name: 'Torta de Frango', amount: -10},
+    { id: 4, name: 'Aula de Violão', amount: 150}
+]
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li =document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+    `
+
+    transactionUl.append(li)
+
+}
+
+const init = () => {
+    dummyTransaction.forEach(addTransactionIntoDOM)
+}
+
+init()
